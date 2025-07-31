475b938027555d3ac21c72c202fb98942ef25568
ajaybenii <ajayben4698@gmail.com>
2025-07-31 17:25:29 +0530

    hide dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load .env file
+// require('dotenv').config(); // Load .env file
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
